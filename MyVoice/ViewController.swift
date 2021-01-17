caab54594ea95836f8d71b4ebb046de28a499881
tdobrowolski <tobdob15@gmail.com>
2021-01-17 20:14:54 +0100

    Initial Commit

---
+++ MyVoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyVoice
+//
+//  Created by Tobiasz Dobrowolski on 17/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
